a7ec2ff15a27f18fd52e2710e9cad4dbcbb62a6f
mcculls5 <mcculls5@wwu.edu>
2021-01-30 13:49:34 -0800

    removed data loading, moved all non-connectome specific functions to math_util

--- nonnegative_connectome.py
+++ nonnegative_connectome.py
@@ -1,16 +1,18 @@
 import numpy as np
-from typing import Callable
-import alt_acc_prox_grad
-import load_mat
 import scipy
+import math_util
+import alt_acc_prox_grad
+
+
 
 
 def optimize_alt_pgd(U, V,
                     X, Y, Lx, Ly, Omega,
-                    lamb, 
-                    max_outer_iter,
-                    max_inner_iter,
-                    max_line_iter,
+                    lamb,
+                    tol=1e-10,
+                    max_outer_iter=10,
+                    max_inner_iter=10,
+                    max_line_iter=100,
                     calculate_cost=True):
 
 
@@ -20,8 +22,9 @@ def optimize_alt_pgd(U, V,
                     lambda U, V: regularized_cost(U, V.T, X, Y, Lx, Ly, lamb, Omega),
                     lambda U, V: grad_cost_U(U, V.T, X, Y, Lx, Ly, lamb, Omega),
                     lambda U, V: grad_cost_V(U, V.T, X, Y, Lx, Ly, lamb, Omega).T, # note transpose
-                    indicator_positive,
-                    proj_nonnegative,
+                    math_util.indicate_positive,
+                    math_util.proj_nonnegative,
+                    tol,
                     max_outer_iter,
                     max_inner_iter,
                     max_line_iter,
@@ -33,8 +36,6 @@ def P_Omega(X, Omega):
     # print("P_omega")
     X[Omega.toarray()==1] = 0
     return X
-
-
 
 
 def grad_cost_U(U, V, X, Y, Lx, Ly, lamb, Omega):
@@ -64,29 +65,11 @@ def grad_cost_V(U ,V ,X, Y, Lx, Ly, lamb, Omega):
 def regularized_cost(U, V, X, Y, Lx, Ly, lamb, Omega):
     # print("regularized_cost", U.shape, V.shape, X.shape, Y.shape, Lx.shape, Ly.shape, Omega.shape)
     result = np.linalg.norm(P_Omega(U @ (V.T @ X) - Y, Omega), ord='fro')**2
-    result = result + lamb * frobenius_squared_trace(Ly @ U, V.T, U, V.T @ Lx.T)
+    result = result + lamb * math_util.factorized_difference_frobenius_sq(Ly @ U, V.T, U, V.T @ Lx.T)
 
     return result
 
-# Return maximum cost if X has any elements < 0
-def indicator_positive(X):
-    if(X[X<0].size > 0):
-        return np.finfo(X.dtype).max
-    else:
-        return 0
-
-# Project Matrix X to nonnegative by setting all negative elements to 0.
-# step size parameter 's' ignored (required for use as prox argument in for acc_prox_grad_method)
-def proj_nonnegative(X, s=None):
-    X[X<0] = 0
-    return X
 
 
-# ||YZ+WH||F^2 using trace
-def frobenius_squared_trace(W, H, Y, Z):
-    # print("frobenius_squared_trace", W.shape, H.shape, Y.shape, Z.shape)
-    trace1 = np.trace(H @ H.T @ W.T @ W)
-    trace2 = np.trace(Z @ Z.T @ Y.T @ Y)
-    trace3 = np.trace(H @ Z.T @ Y.T @ W)
-    return trace1 + trace2 + 2*trace3
+
 
