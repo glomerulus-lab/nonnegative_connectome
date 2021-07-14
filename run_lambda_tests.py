2c8fa4f53828708e1d2976c3cf97e9b330fce6be
Jessica-Stillwell <stillwj3@wwu.edu>
2021-07-14 13:14:15 -0700

    Cleaned up code

--- run_lambda_tests.py
+++ run_lambda_tests.py
@@ -1,11 +1,13 @@
 import os
 import glob
-filename = []
 
+filename = []
 for filepath in glob.glob('/home/stillwj3/Documents/research/lowrank_connectome/matlab/solution/lambda_test_*.mat'):
     base = os.path.basename(filepath)
     filename = os.path.splitext(base)
+
     path = os.path.join("lambda_tests/lambda_images/", filename[0])
-    # os.mkdir(path)
-    command = "python nonnegative_converter.py test " + filename[0] + " lambda_tests/lambda_data lambda_tests/lambda_images/"+filename[0] + " nonneg_"+filename[0] + " 400 14 50 400 14 50 -init_tol 1e-6 -tol 1e-7 --load_lamb -from_lc"
+    os.mkdir(path)
+
+    command = "python nonnegative_converter.py test " + filename[0] + " lambda_tests/lambda_data/ lambda_tests/lambda_images/"+filename[0]+"/" +" "+ filename[0].replace("test_","") + " 400 14 50 400 14 50 -init_tol 1e-6 -tol 1e-7 --load_lamb -from_lc"
     os.system(command)
