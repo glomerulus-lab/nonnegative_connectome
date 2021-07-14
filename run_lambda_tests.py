df1cd9b830c8573845f30972116980839afc7cfe
Jessica-Stillwell <stillwj3@wwu.edu>
2021-07-14 09:55:26 -0700

    Iterates over greedy intializations and creates command using filenames

--- run_lambda_tests.py
+++ run_lambda_tests.py
@@ -1,11 +1,11 @@
 import os
 import glob
+filename = []
 
 for filepath in glob.glob('/home/stillwj3/Documents/research/lowrank_connectome/matlab/solution/lambda_test_*.mat'):
     base = os.path.basename(filepath)
     filename = os.path.splitext(base)
     path = os.path.join("lambda_tests/lambda_images/", filename[0])
-    os.mkdir(path)
-
-    #python main.py test filename[0] lambda_tests/lambda_data/ lambda_tests/lambda_images/"$i"_images/ "$i" 400 14 50 400 14 50 -init_tol 1e-6 -tol 1e-7 -lamb $i --load_lambda
-
+    # os.mkdir(path)
+    command = "python nonnegative_converter.py test " + filename[0] + " lambda_tests/ lambda_tests/ " + "test_load_lamb" + " 400 14 50 400 14 50 -init_tol 1e-6 -tol 1e-7 --load_lamb -from_lc"
+    os.system(command)
