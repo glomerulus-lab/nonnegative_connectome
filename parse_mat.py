307e8925ac00053a2c70f317585cf66b5cb4c526
Jessica-Stillwell <stillwj3@wwu.edu>
2021-07-14 12:44:25 -0700

    iterates over files to plot l curve

--- parse_mat.py
+++ parse_mat.py
@@ -2,44 +2,40 @@ import scipy.io as sci
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib as mpl
+import glob
 
+# get lambda values used for greedy initialization
+Lambdas= []
+for filepath in glob.glob('/home/stillwj3/Documents/research/lowrank_connectome/matlab/lambda_tests*.mat'):
+    data = sci.loadmat(filepath)
+    Lambdas.append(data["Lambdas"][0])
+
+# parse data for each lambda test
 losses = []
 regs = []
 costs = []
-lambdas = [-2,-1,0,1,2,3,4,5,6,7,8,9,10]
-# parse data for each lambda test
-for x in range(-2,11):
-    # Create filename
-    if x != 0:
-        lamb = "1e"+str(x)
-    else:
-        lamb = "0"
-    filename = "test_" + lamb
-
-    # Load and save data
-    data = sci.loadmat("lambda_tests/lambda_data/"+filename, appendmat=True)
+for filepath in glob.glob('lambda_tests/lambda_data/test*.mat'):
+    print(filepath)
+    data = sci.loadmat(filepath)
     costs.append(data["cost_final"][0][0])
     losses.append(data["loss_final"][0][0])
     regs.append(data["reg_final"][0][0])
 
-# print(regs)
-# print(losses)
-
 # plotting l-curve (reg, loss)
 plt.plot(regs, losses, linestyle="-", color='b')
-plt.scatter(regs, losses, 50, lambdas)
+plt.scatter(regs, losses, 50, Lambdas)
 
 # add colorbar and labels
 cmap = mpl.cm.viridis
-bounds = lambdas
+bounds = Lambdas[0]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), label="Lambda")
 plt.xlabel("Regularization")
 plt.ylabel("Loss")
-# plt.show()
+plt.show()
 
-# save l-curve
-plt.savefig("lambda_tests/1e-2_nonneg-l-curve.svg")
-plt.savefig("lambda_tests/1e-2_nonneg-l-curve.jpg")
+# # save l-curve
+# plt.savefig("lambda_tests/test-l-curve.svg")
+# plt.savefig("lambda_tests/test-l-curve.jpg")
 
 
