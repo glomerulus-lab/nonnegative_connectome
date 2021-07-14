9967108eb74a6651222960d4b2c5acde50cf8522
Jessica-Stillwell <stillwj3@wwu.edu>
2021-07-14 09:58:19 -0700

    updated folders where new data is saved and changed filename of new data

--- run_lambda_tests.py
+++ run_lambda_tests.py
@@ -7,5 +7,5 @@ for filepath in glob.glob('/home/stillwj3/Documents/research/lowrank_connectome/
     filename = os.path.splitext(base)
     path = os.path.join("lambda_tests/lambda_images/", filename[0])
     # os.mkdir(path)
-    command = "python nonnegative_converter.py test " + filename[0] + " lambda_tests/ lambda_tests/ " + "test_load_lamb" + " 400 14 50 400 14 50 -init_tol 1e-6 -tol 1e-7 --load_lamb -from_lc"
+    command = "python nonnegative_converter.py test " + filename[0] + " lambda_tests/lambda_data lambda_tests/lambda_images/"+filename[0] + " nonneg_"+filename[0] + " 400 14 50 400 14 50 -init_tol 1e-6 -tol 1e-7 --load_lamb -from_lc"
     os.system(command)
