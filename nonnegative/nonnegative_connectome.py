58d1db8df40c866ac82186df8f46027b6397809f
Jessica-Stillwell <stillwj3@wwu.edu>
2021-07-22 12:39:29 -0700

    added import statements

--- nonnegative/nonnegative_connectome.py
+++ nonnegative/nonnegative_connectome.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.linalg
+import sys, inspect, os
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
--- nonnegative/nonnegative_initialization.py
+++ nonnegative/nonnegative_initialization.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys, inspect, os
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
