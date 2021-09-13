b7c470ba4ce565e131a2664f0bcc721e1dbc7516
Jessica-Stillwell <stillwj3@wwu.edu>
2021-09-13 14:35:17 -0700

    Added mark to optimal lambda and added comments

--- plot/plot_l_curve.py
+++ plot/plot_l_curve.py
@@ -1,17 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-import matplotlib as mpl
 import argparse
 import glob
-import math 
 import copy 
-from plot_l import plot 
+from plot_curve import plot 
 import sys, inspect, os
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 from util.parse import parse
-from util.sort import sort_vals
+from util.sort import sort_vals, sort_vals2
 
 
 
@@ -24,23 +22,42 @@ parser.add_argument('path_to_solution',      type=str, nargs=1, help='Path to .m
 parser.add_argument('title',                 type=str, nargs=1, help='Title of l-curve.')
 # Flags
 parser.add_argument('-nneg', action='store_true', help='Determines key used to get lambda value. When nonnegative solution used: nneg=True.')
+  
 
-
-## Plot the reg, loss l-curve for specified solutions
-    # Input:
-        # testname: 'test', 'top_view' or 'flatmap' (str)
-        # path: path to solution (str)
-        # name: filename of solution (str)
-        # title: figure title (str)
-        # greedy: is greedy soltion (bool)
 def create_l_curve(testname, path, name, title, greedy):
-    # get data and sort it
+    '''
+    Saves a regularization vs. loss l-curve given a specific solution.
+
+    Parameters:
+        testname (str) : 'test', 'top_view' or 'flatmap'
+        path (str) : path to solution
+        name (str) : filename of solution
+        title (str) : title of l-curve
+        greedy (bool) : is greedy solution
+    '''
+
+    # Get data and sort it
     losses, regs, costs, lambs = parse(path+name, greedy)
     lambs_copy = copy.deepcopy(lambs)
-    lambs_copy, regs = sort_vals(lambs_copy,regs)
+    lambs_copy, regs = sort_vals2(lambs_copy,regs)
     lambs, losses = sort_vals(lambs,losses)
-    
-    plt = plot(regs, losses, "Regularization", "Loss", title.replace("_", " "), lambs, r'$\lambda$')
+  
+    plt = plot(regs, losses, "Regularization", "Loss", title.replace("_", " "), lambs, r'$\lambda$', lambs_copy[0],lambs_copy[len(lambs_copy)-1])
+
+    # Mark optimal lambda with X
+    px=0
+    py=0
+    for i in range(len(lambs)):
+        if(testname == 'flatmap' and lambs[i]==7):
+            px = regs[i]
+            py = losses[i]
+        elif(lambs[i]==4 and not testname == 'flatmap'):
+            px = regs[i]
+            py = losses[i]
+
+    plt.plot(px,py, 'x', color='tomato', zorder=100,mew=5, markersize=12) 
+    plt.tight_layout()
+
     # save l-curve
     if(testname == 'test'):
         path = "../data/lambda_tests/"
@@ -49,19 +66,16 @@ def create_l_curve(testname, path, name, title, greedy):
     elif(testname == 'flatmap'):
         path = "../data/lambda_fm/"
 
-    plt.savefig(path+title.lower()+".jpg")
-    plt.savefig(path+title.lower()+".svg")
-    return plt
+    plt.savefig(path+title.lower()+".png", dpi=600)
 
 if __name__ == '__main__':
-
     args = parser.parse_args()
     print('solution_name: ', args.solution_name[0], '\npath_to_solution: ', args.path_to_solution[0], '\ntitle: ', args.title[0])
     if(args.nneg):
         print('begin nonnegative l-curve')
         create_l_curve(args.testname[0], args.path_to_solution[0], args.solution_name[0], args.title[0], False)
+
     else:
         print('begin greedy l-curve')
         create_l_curve(args.testname[0], args.path_to_solution[0], args.solution_name[0], args.title[0], True)
-
 
